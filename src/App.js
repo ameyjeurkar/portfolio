c7c23732eb4b237feae2cb473bee529a3694c837
Amey Jeurkar <jeurkaramey@gmail.com>
2022-07-01 10:51:45 +0530

    Removed unused component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import AboutMe from './components/AboutMe/AboutMe';
 import Footer from './components/Footer/Footer';
 import MainPage from './components/MainPage/MainPage';
-import Portfolio from './components/Portfolio/Portfolio';
 import PortfolioContainer from './components/PortfolioContainer/PortfolioContainer';
 import Skills from './components/Skills/Skills';
 
