87eb4c59b5ba2ab8e2117cb2811ac2bdfcea52ef
ahhmay <jeurkaramey@gmail.com>
2023-07-08 13:16:55 +0530

    fix: About Me Section Revamped.

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,39 +1,35 @@
 import React from 'react';
-import AboutMePic from './../../assets/images/aboutMe_4x3.jpg';
 import ResumePdf from './../../assets/json/ameyjeurkar_resume.pdf';
 import './AboutMe.css';
 
 const AboutMe = () => {
-
     const downloadFile = () => {
       window.location.href = "./../../assets/json/ameyjeurkar_resume.pdf";
     };
 
     const calculateYearsofExperience = (startDate, endDate) => {
-      let monthsOfExperience =  endDate.getMonth() - startDate.getMonth() + 12*(endDate.getFullYear() - startDate.getFullYear());
+      let monthsOfExperience =  endDate.getMonth() - startDate.getMonth() + 12*(endDate.getFullYear() - startDate.getFullYear()) + 6;
       let convertMonthsToYears = parseInt(monthsOfExperience/12)
       let monthsMissed = monthsOfExperience%12;
-      return `${convertMonthsToYears}${monthsMissed>6 ? '.' : ''}${monthsMissed>6 ? 5 : ''}years`;
+      return `${convertMonthsToYears}${monthsMissed>0 ? '.' : ''}${monthsMissed>0 ? monthsMissed : ''}`;
     }
 
     return (
-      <div id="aboutMeSection" className="about-container container-fluid px-0 px-5">
+      <div id="aboutMeSection" className="about-container container-fluid px-4">
         <div className="row d-flex justify-content-center my-3">
-          <img src={AboutMePic} alt="About Me Pic" className="col-lg-6 col-md-6 col-sm-12 about-me-pic" />
-          <div className='col-lg-6 col-md-6 col-sm-12'>
+          <div className='col-lg-8 col-md-8 col-sm-12'>
             <div className="about-me-heading mx-0">
-                <p>About Me</p>
+              <p>About Me</p>
             </div>
             <div className="about-me-description mx-0">
               <p>
-              I'm a enthusiastic and motivated MERN/MEAN Stack Developer with more than {calculateYearsofExperience(new Date('2020-12-14'), new Date())} of 
-              industry experience working in the 
-              frontend & backend technologies, building 
-              industry-grade web-applications. Proficient in technologies like
-              React.js, Angular2+, Express, MongoDB Database. I have maintained 
-              and developed multiple projects from 
-              scratch, carrying the development of its 
-              frontend & backend codebase from scratch.
+                Welcome to my portfolio! I am an enthusiastic and dedicated MERN Full Stack developer with a passion for creating impactful and innovative web applications. Over the course of my <span className='experienceYears'>{calculateYearsofExperience(new Date('2020-12-14'), new Date())}</span>years journey in the world of software development, I have gained valuable experience and expertise that has shaped me into the professional I am today.
+              </p>
+              <p>
+                I have a strong belief in lifelong learning, especially in the fast-paced world of technology. I thrive on exploring new technologies and staying updated with the latest advancements. Whether it's diving into new frameworks, embracing emerging libraries, or delving into cloud technologies, I am always eager to expand my skill set and take on new challenges.
+              </p>
+              <p>
+              I am excited about the possibility of collaborating with talented individuals like yourself to create something truly extraordinary. I look forward to connecting with you soon and embarking on this amazing journey together!
               </p>
             </div>
             <div className="mx-0">
