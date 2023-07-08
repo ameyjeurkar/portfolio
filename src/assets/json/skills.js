5e8840dddcbdff109d5db77571c88417cd419591
ahhmay <jeurkaramey@gmail.com>
2023-07-08 15:49:30 +0530

    feat: Skills Section Revamped

---
+++ src/assets/json/skills.js
@@ -0,0 +1,131 @@
+import ReactJS from "./../images/skills_logo/react.png";
+import Redux from "./../images/skills_logo/redux.png";
+import AWS from "./../images/skills_logo/aws.png";
+import Bootstrap from "./../images/skills_logo/bootstrap.png";
+import CSS from "./../images/skills_logo/css3.png";
+import Express from "./../images/skills_logo/express.png";
+import Html from "./../images/skills_logo/html5.png";
+import Github from "./../images/skills_logo/github.png";
+import Javascript from "./../images/skills_logo/javascript.png";
+import Mongodb from "./../images/skills_logo/mongodb.png";
+import Nodejs from "./../images/skills_logo/nodejs.png";
+import NPM from "./../images/skills_logo/npm.png";
+import SASS from "./../images/skills_logo/sass.png";
+import Tailwind from "./../images/skills_logo/tailwind-css.png";
+import Typescript from "./../images/skills_logo/typescript.png";
+import Webpack from "./../images/skills_logo/webpack.png";
+
+export const SkillJSON = [
+    {
+        "id": "react",
+        "skillName": "ReactJS",
+        "skillIcon": ReactJS,
+        "skillType": "frontend",
+        "displaySequence": 1
+    },
+    {
+        "id": "redux",
+        "skillName": "Redux",
+        "skillIcon": Redux,
+        "skillType": "frontend",
+        "displaySequence": 2
+    },
+    {
+        "id": "aws",
+        "skillName": "AWS",
+        "skillIcon": AWS,
+        "skillType": "backend",
+        "displaySequence": 12
+    },
+    {
+        "id": "bootstrap",
+        "skillName": "Bootstrap",
+        "skillIcon": Bootstrap,
+        "skillType": "frontend",
+        "displaySequence": 8
+    },
+    {
+        "id": "css",
+        "skillName": "CSS",
+        "skillIcon": CSS,
+        "skillType": "frontend",
+        "displaySequence": 6
+    },
+    {
+        "id": "express",
+        "skillName": "ExpressJS",
+        "skillIcon": Express,
+        "skillType": "backend",
+        "displaySequence": 10
+    },
+    {
+        "id": "github",
+        "skillName": "Github",
+        "skillIcon": Github,
+        "skillType": "other",
+        "displaySequence": 13
+    },
+    {
+        "id": "html",
+        "skillName": "HTML5",
+        "skillIcon": Html,
+        "skillType": "frontend",
+        "displaySequence": 5
+    },
+    {
+        "id": "javascript",
+        "skillName": "Javascript",
+        "skillIcon": Javascript,
+        "skillType": "frontend",
+        "displaySequence": 3
+    },
+    {
+        "id": "mongodb",
+        "skillName": "MongoDB",
+        "skillIcon": Mongodb,
+        "skillType": "backend",
+        "displaySequence": 11
+    },
+    {
+        "id": "nodejs",
+        "skillName": "NodeJS",
+        "skillIcon": Nodejs,
+        "skillType": "backend",
+        "displaySequence": 9
+    },
+    {
+        "id": "npm",
+        "skillName": "NPM",
+        "skillIcon": NPM,
+        "skillType": "other",
+        "displaySequence": 14
+    },
+    {
+        "id": "sass",
+        "skillName": "SASS",
+        "skillIcon": SASS,
+        "skillType": "frontend",
+        "displaySequence": 7
+    },
+    {
+        "id": "tailwind",
+        "skillName": "Tailwind CSS",
+        "skillIcon": Tailwind,
+        "skillType": "frontend",
+        "displaySequence": 8
+    },
+    {
+        "id": "typescript",
+        "skillName": "Typescript",
+        "skillIcon": Typescript,
+        "skillType": "frontend",
+        "displaySequence": 4
+    },
+    {
+        "id": "webpack",
+        "skillName": "Webpack",
+        "skillIcon": Webpack,
+        "skillType": "other",
+        "displaySequence": 15
+    },
+];
--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,74 +1,62 @@
-import React, { useState } from 'react';
-import {
-    Chart as ChartJS,
-    RadialLinearScale,
-    PointElement,
-    LineElement,
-    Filler,
-    Tooltip,
-    Legend,
-  } from 'chart.js';
-import { Radar } from 'react-chartjs-2';
+import React from 'react';
+import { SkillJSON } from '../../assets/json/skills';
 import './Skills.css';
 
-ChartJS.register(
-    RadialLinearScale,
-    PointElement,
-    LineElement,
-    Filler,
-    Tooltip,
-    Legend
-);
 
 const Skills = () => {
-    const [isHovered, setIsHovered] = useState(false);
-    const RadarOptions = {
-        scale: {
-            r: {
-                angleLines: {
-                    display: false
-                },
-                suggestedMin: 0,
-                suggestedMax: 10
-            },
-            angleLines: {
-                color: `${isHovered ? 'white' : 'white'}`,
-                lineWidth: 1
-            },
-            gridLines: {
-                color: `${isHovered ? 'white' : 'white'}`,
-                circular: true
-            }
-        }
-    }
-    
-    const data = {
-        labels: ['React', 'Angular', 'Javascript', 'Node.js', 'Expressjs', 'MongoDB'],
-        datasets: [
-            {
-                label: 'Proficiency',
-                data: [8, 7, 7, 7, 7, 8],
-                fill: true,
-                backgroundColor: 'rgb(212,59,64, 0.7)',
-                borderColor: `${isHovered ? 'white':'white'}`,
-                hoverBorderColor: `${isHovered ? 'white':'white'}`,
-                borderWidth: 2
-            }
-        ],
-    };
-
-    const handleHover = (value) => {
-        setIsHovered(value)
-    }
 
     return (
-        <div id="skillsSection" className="container-fluid py-2 fade-in-hover-animation" onMouseOver={() => handleHover(true)} onMouseOut={() => handleHover(false)}>
+        <div id="skillsSection" className="container-fluid py-2 fade-in-hover">
             <div className="row">
                 <div className="col-12">
                     <p className="skill-title mb-0 mt-3">Skills</p>
                 </div>
-                <div className="col-12 skill-container d-flex margin-auto">
-                    <Radar data={data} options={RadarOptions}/>
+                <div className="col-12 d-flex pb-2 fade-in-hover">
+                    <div className="col-2 skill-type">FRONTEND</div>
+                    <div className="col-10">
+                        {
+                            SkillJSON.map(skill => {
+                                return (
+                                    skill?.skillType==="frontend" && 
+                                    <>
+                                        <img className="skill-icon" src={skill.skillIcon} alt={skill.skillName}/>
+                                    </>
+                                )
+                            })
+                        }
+                    </div>
+                </div>
+
+                <div className="col-12 d-flex pb-2">
+                    <div className="col-2 skill-type">Backend</div>
+                    <div className="col-10">
+                        {
+                            SkillJSON.map(skill => {
+                                return (
+                                    skill?.skillType==="backend" && 
+                                    <>
+                                        <img className="skill-icon" src={skill.skillIcon} alt={skill.skillName}/>
+                                    </>
+                                )
+                            })
+                        }
+                    </div>
+                </div>
+
+                <div className="col-12 d-flex">
+                    <div className="col-2 skill-type">Others</div>
+                    <div className="col-10">
+                        {
+                            SkillJSON.map(skill => {
+                                return (
+                                    skill?.skillType==="other" && 
+                                    <>
+                                        <img className="skill-icon" src={skill.skillIcon} alt={skill.skillName}/>
+                                    </>
+                                )
+                            })
+                        }
+                    </div>
                 </div>
             </div>
         </div>
